f9a7abe4e335aa7a6e57ab916ad92f6a8b22b33e
shakirov-aa <almick17933@gmail.com>
2020-04-22 14:20:31 +0500

    navigableMap changed

--- src/main/java/ru/mail/polis/shakirov/DAOImpl.java
+++ src/main/java/ru/mail/polis/shakirov/DAOImpl.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.shakirov;
 
+import com.google.common.collect.Iterators;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.DAO;
 import ru.mail.polis.Record;
@@ -10,17 +11,17 @@ import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class DAOImpl implements DAO {
-    private final NavigableMap<ByteBuffer, Record> db = new TreeMap<>();
+    private final NavigableMap<ByteBuffer, ByteBuffer> db = new TreeMap<>();
 
     @NotNull
     @Override
     public Iterator<Record> iterator(@NotNull final ByteBuffer from) {
-        return db.tailMap(from).values().iterator();
+        return Iterators.transform(db.tailMap(from).entrySet().iterator(), item -> Record.of(item.getKey(), item.getValue()));
     }
 
     @Override
     public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) {
-        db.put(key, Record.of(key, value));
+        db.put(key, value);
     }
 
     @Override
