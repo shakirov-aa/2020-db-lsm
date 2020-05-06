87ba53f3cc678c4a24dfe17fa799cb81ea4da1ef
shakirov-aa <almick17933@gmail.com>
2020-05-06 14:19:04 +0500

    printStackTrace replaced by logger

--- src/main/java/ru/mail/polis/shakirov/LsmDao.java
+++ src/main/java/ru/mail/polis/shakirov/LsmDao.java
@@ -2,6 +2,8 @@ package ru.mail.polis.shakirov;
 
 import com.google.common.collect.Iterators;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.mail.polis.DAO;
 import ru.mail.polis.Iters;
 import ru.mail.polis.Record;
@@ -20,6 +22,8 @@ import java.util.TreeMap;
 import java.util.stream.Stream;
 
 public class LsmDao implements DAO {
+    private static final Logger log = LoggerFactory.getLogger(LsmDao.class);
+
     private static String SUFFIX = ".dat";
     private static String TEMP = ".tmp";
 
@@ -53,7 +57,7 @@ public class LsmDao implements DAO {
                     ssTables.put(gen, new SSTable(f.toFile()));
                 } catch (IOException e) {
                     // Log bad file
-                    e.printStackTrace();
+                    log.info("IOException in 'new SSTable'");
                 }
             });
         }
